9348fcc32fa54e589ff4ce207442ea07ac7e9cdc
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-04 14:25:08 +0630

    initialized web startup

--- MemberApp.Web/Startup.cs
+++ MemberApp.Web/Startup.cs
@@ -1,12 +1,12 @@
+using MemberApp.Data;
+using MemberApp.Data.Abstract;
+using MemberApp.Data.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MemberApp.Web
 {
@@ -22,6 +22,14 @@ namespace MemberApp.Web
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            string connectionString = Configuration.GetConnectionString("MemberAppDatabase");
+
+            services.AddDbContextPool<MemberAppContext>(options =>
+                options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString), builder => builder.MigrationsAssembly("MemberApp.Web")));
+
+            // Repositories
+            services.AddScoped<IMemberRepository, MemberRepository>();
+
             services.AddControllersWithViews();
         }
 
