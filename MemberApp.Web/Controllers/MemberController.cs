cb5b8546ef8fb07a9f05ee15397b1a33fe897ec3
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-13 01:38:21 +0630

    member list create actions

--- MemberApp.Web/Controllers/MemberController.cs
+++ MemberApp.Web/Controllers/MemberController.cs
@@ -1,11 +1,15 @@
 ﻿using MemberApp.Data.Abstract;
+using MemberApp.Data.Infrastructure.Services.Abstract;
 using MemberApp.Model.Entities;
 using MemberApp.Web.ViewModels.Members;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace MemberApp.Web.Controllers
@@ -14,14 +18,22 @@ namespace MemberApp.Web.Controllers
     public class MemberController : BaseController
     {
         private readonly IRepository<Member> _memberRepository;
-
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+        private readonly ISmsService _smsService;
 
         public MemberController(
             IRepository<AuditLog> loggingRepository,
-            IRepository<Member> memberRepository)
+            UserManager<ApplicationUser> userManager,
+            RoleManager<IdentityRole> roleManager,
+            IRepository<Member> memberRepository,
+            ISmsService smsService)
             : base(loggingRepository)
         {
             _memberRepository = memberRepository;
+            _userManager = userManager;
+            _roleManager = roleManager;
+            _smsService = smsService;
         }
 
         [HttpGet]
@@ -41,6 +53,130 @@ namespace MemberApp.Web.Controllers
                                        .ToListAsync();
 
             return View(viewModel);
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Create()
+        {
+            ViewBag.Roles = await _roleManager.Roles.ToListAsync();
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Create(MemberCreateViewModel viewModel)
+        {
+            try
+            {
+                ViewBag.Roles = await _roleManager.Roles.ToListAsync();
+
+                if (!ModelState.IsValid)
+                    return View(viewModel);
+
+                var exists = await _userManager.FindByNameAsync(viewModel.PhoneNumber);
+
+                if (exists != null)
+                {
+                    GenerateAlertMessage(false, "The phone number already exists");
+                    return View(viewModel);
+                }
+
+                var user = new ApplicationUser
+                {
+                    UserName = viewModel.PhoneNumber,
+                    PhoneNumber = viewModel.PhoneNumber,
+                    PhoneNumberConfirmed = true,
+                    Email = viewModel.Email,
+                    EmailConfirmed = true,
+                    CreatedDate = DateTime.UtcNow,
+                    UpdatedDate = DateTime.UtcNow,
+                    Status = true
+                };
+
+                string password = GeneratePassword();
+                var result = await _userManager.CreateAsync(user, password);
+
+                if (!result.Succeeded)
+                {
+                    foreach (var error in result.Errors)
+                    {
+                        ModelState.AddModelError(string.Empty, error.Description);
+                    }
+
+                    GenerateAlertMessage(false, "The member is failed to create");
+
+                    return View(viewModel);
+                }
+
+                var role = await _roleManager.FindByIdAsync(viewModel.Role);
+
+                if (role != null)
+                {
+                    await _userManager.AddToRoleAsync(user, role.Name);
+                }
+
+                var member = new Member
+                {
+                    ApplicationUserId = user.Id,
+                    FullName = viewModel.FullName
+                };
+
+                await _memberRepository.AddAsync(member);
+                await _memberRepository.CommitAsync();
+
+                await _smsService.SendSMSAsync(user.PhoneNumber, $"Your password for member app is {password}.");
+
+                GenerateAlertMessage(true, "The member is created successfully.");
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch (Exception ex)
+            {
+                GenerateAlertMessage(false, ex.Message);
+                return View(viewModel);
+            }
+        }
+
+        public string GeneratePassword()
+        {
+            var options = _userManager.Options.Password;
+
+            int length = options.RequiredLength;
+
+            bool digit = options.RequireDigit;
+            bool lowercase = options.RequireLowercase;
+            bool uppercase = options.RequireUppercase;
+            bool nonAlphanumeric = options.RequireNonAlphanumeric;
+
+            var password = new StringBuilder();
+            var random = new Random();
+
+            while (password.Length < length)
+            {
+                char c = (char)random.Next(32, 126);
+
+                password.Append(c);
+
+                if (char.IsDigit(c))
+                    digit = false;
+                else if (char.IsLower(c))
+                    lowercase = false;
+                else if (char.IsUpper(c))
+                    uppercase = false;
+                else if (!char.IsLetterOrDigit(c))
+                    nonAlphanumeric = false;
+            }
+
+            if (nonAlphanumeric)
+                password.Append((char)random.Next(33, 48));
+            if (digit)
+                password.Append((char)random.Next(48, 58));
+            if (lowercase)
+                password.Append((char)random.Next(97, 123));
+            if (uppercase)
+                password.Append((char)random.Next(65, 91));
+
+            return password.ToString();
         }
     }
 }
---
+++ MemberApp.Web/ViewModels/Members/MemberCreateViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MemberApp.Web.ViewModels.Members
+{
+    public class MemberCreateViewModel
+    {
+        [Required]
+        public string FullName { get; set; }
+
+        [Required]
+        [Phone]
+        public string PhoneNumber { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        public string Role { get; set; }
+    }
+}
