8a5c13bc68833b63aea2f29935ab7fb589284dd1
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-18 23:45:47 +0630

    add notification menu controller and view

---
+++ MemberApp.Web/Controllers/NotificationController.cs
@@ -0,0 +1,23 @@
+﻿using MemberApp.Data.Abstract;
+using MemberApp.Model.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemberApp.Web.Controllers
+{
+    [Authorize(Policy = "AdminOnly")]
+    public class NotificationController : BaseController
+    {
+        public NotificationController(
+            IRepository<AuditLog> loggingRepository) 
+            : base(loggingRepository)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
