c7e207e9a9972738f96c2827c4c7ecc456643149
Thiha <thihakyawhtin.mm@gmail.com>
2022-02-19 18:08:50 +0630

    mini sidebar and setting menu

---
+++ MemberApp.Web/Controllers/SettingController.cs
@@ -0,0 +1,23 @@
+﻿using MemberApp.Data.Abstract;
+using MemberApp.Model.Entities;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MemberApp.Web.Controllers
+{
+    [Authorize(Policy = "AdminOnly")]
+    public class SettingController : BaseController
+    {
+        public SettingController(
+            IRepository<AuditLog> loggingRepository)
+            : base(loggingRepository)
+        {
+        }
+
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
